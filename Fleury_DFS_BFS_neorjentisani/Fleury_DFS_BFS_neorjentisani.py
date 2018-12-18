a472ed2f979f4291421048d708f0b0b495ea3cf6
Alex <m.aleksandar1994@gmail.com>
2018-12-18 01:22:04 +0100

    DFS, izmenjen ispis

--- Fleury_DFS_BFS_neorjentisani/Fleury_DFS_BFS_neorjentisani.py
+++ Fleury_DFS_BFS_neorjentisani/Fleury_DFS_BFS_neorjentisani.py
@@ -2,7 +2,6 @@ import numpy as np
 from collections import defaultdict
 import sys
 
-
 class Graf:
 
     def __init__(self, cvorovi):
@@ -13,6 +12,19 @@ class Graf:
         self.graf[u].append(v)
         self.graf[v].append(u)
 
+    def DFS_pom(self, c, posecen):
+
+        posecen[c] = True
+        print(c, end=" ")
+
+        for i in self.graf[c]:
+            if not posecen[i]:
+                self.DFS_pom(i, posecen)
+
+    def DFS_neorj(self, c):
+
+        posecen = [False] * len(self.graf)
+        self.DFS_pom(c, posecen)
 
     def BFS_neorj(self, c):
 
@@ -57,6 +69,7 @@ def ispis(Q, posecen, p, l, t):
     sp = " "
     br1 = 0
     br2 = 0
+
     for i in p:
         if i != '-':
             br1 += 1
@@ -65,7 +78,11 @@ def ispis(Q, posecen, p, l, t):
         if i != '-':
             br2 += 1
 
-    print(Q, sp*3*(len(posecen)-len(Q)), posecen, sp, p, br1*2*sp, l, br2*2*sp, t)
+    if len(p) < 10:
+        print(Q, sp*3*(len(posecen)-len(Q)), posecen, sp, p, br1*2*sp, l, br1*2*sp, t)
+    else:
+        print(Q, "\n", posecen, "\n", p, "\n", l, "\n", t)
+        print("----------")
 
 
 def unesi_graf():
@@ -76,19 +93,20 @@ def unesi_graf():
     for linija in sys.stdin:
 
         linija = linija.strip().split(" ")
-        print("grana: ", linija[0], linija[1])
         graf.dodaj_granu(int(linija[0]), int(linija[1]))
 
     return graf
 
 
-
-
-
 def main():
 
     g = unesi_graf()
+    print("\n############# BFS #############")
     g.BFS_neorj(0)
+    print("############# DFS #############")
+    g.DFS_neorj(0)
+    print("\n")
+
 
 if __name__ == '__main__':
     main()
